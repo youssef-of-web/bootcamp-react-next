515f4760aee1d0a56d979831ac64025fa1d19803
Mansouri youssef <mansouriyoussef1991@gmail.com>
2025-05-28 21:58:10 +0100

    Hotfix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || '/bootcamp-react-next',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
