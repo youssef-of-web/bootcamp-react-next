794d9c94b929d5a7a74c2463ae17f40a1c5044b3
Mansouri youssef <mansouriyoussef1991@gmail.com>
2025-06-02 12:49:59 +0100

    Remove base

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+
 import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: process.env.PORT as unknown as number,
+  },
   plugins: [react()],
   resolve: {
     alias: {
